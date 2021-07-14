b34ab766582d15142d6e83a12ae0d7f646942dae
PengMingJiu <pengmingjiu@gmail.com>
2021-07-14 11:56:55 +0800

    chore:add polyfill legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import {defineConfig} from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import legacy from '@vitejs/plugin-legacy'
 import path from "path";
 
 const pathResolve = (dir: string): string => path.resolve(__dirname, dir);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [
+        reactRefresh(),
+        legacy({
+            targets: ['defaults', 'not IE 11']
+        })
+    ],
     publicDir: "public",
     css: {
         preprocessorOptions: {
