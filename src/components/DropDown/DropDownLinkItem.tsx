3edbca1b41aeebd3458f5b754690ee9c14ae8e56
PengMingJiu <pengmingjiu@gmail.com>
2021-08-13 19:31:42 +0800

    chore:update DropDown

--- src/components/DropDown/DropDownLinkItem.tsx
+++ src/components/DropDown/DropDownLinkItem.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode, useContext } from 'react'
 import { ListItem } from '@material-ui/core'
 import { Store } from './DropDown'
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 interface IProps {
     children: ReactNode
@@ -12,19 +12,16 @@ interface IProps {
 
 function DropDownLinkItem(props: IProps) {
     const { setOpen } = useContext(Store)
+    const history = useHistory()
     const onClick = () => {
         if (props.active) return
+        let jump = history.push
+        if (props.replace) jump = history.replace
+        jump(props.to)
         setOpen(false)
     }
     return (
-        <ListItem
-            button
-            component={Link}
-            to={props.to}
-            replace={props.replace || false}
-            selected={props.active}
-            onClick={onClick}
-        >
+        <ListItem button selected={props.active} onClick={onClick}>
             {props.children}
         </ListItem>
     )
