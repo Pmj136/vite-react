1542273d096a5c200fecbd6497b7c4779defb410
PengMingJiu <pengmingjiu@gmail.com>
2021-08-14 20:03:24 +0800

    chore:visual experience optimize

--- src/pages/user/UserInfo/Action.tsx
+++ src/pages/user/UserInfo/Action.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import userStore from '@/store/userStore'
 import { Button } from '@material-ui/core'
 import { observer } from 'mobx-react-lite'
+import { useHistory } from 'react-router-dom'
 
 interface IProps {
     userId: number | undefined
@@ -9,9 +10,19 @@ interface IProps {
 
 function Action(props: IProps) {
     const { uId } = userStore
+    const history = useHistory()
+    const toSetting = () => {
+        history.push('/setting/profile')
+    }
+    if (props.userId === undefined) return null
     if (uId === props.userId)
         return (
-            <Button size="small" variant="outlined" color="primary">
+            <Button
+                size="small"
+                variant="outlined"
+                color="primary"
+                onClick={toSetting}
+            >
                 编辑资料
             </Button>
         )
