3be2e16ae259c6f613d2365bbe46886edb039570
PengMingJiu <pengmingjiu@gmail.com>
2021-08-17 01:10:51 +0800

    chore:remove AutoScrollToTop.tsx

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,7 +2,6 @@ import React, { ReactElement } from 'react'
 import Header from './header/Header'
 import Main from './main/Main'
 import CopyRight from './footer/CopyRight'
-import AutoScrollToTop from './AutoScrollToTop'
 
 interface IProps {
     children: ReactElement
@@ -14,7 +13,6 @@ function Layout(props: IProps) {
             <Header />
             <Main>{props.children}</Main>
             <CopyRight />
-            <AutoScrollToTop selector="html" />
         </>
     )
 }
