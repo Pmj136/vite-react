2ce0b3a98c78d1be907d37cc0f7fe01779a49a4f
PengMingJiu <pengmingjiu@gmail.com>
2021-08-17 01:13:11 +0800

    chore:modify svg classes

--- src/svg/QQ.tsx
+++ src/svg/QQ.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 function Qq() {
     return (
         <svg
-            className="MuiSvgIcon-root MuiSvgIcon-fontSizeLarge"
             viewBox="0 0 1024 1024"
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
