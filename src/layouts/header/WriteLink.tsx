232ff869a88d080ad079159d6c43f4b518e679f1
PengMingJiu <pengmingjiu@gmail.com>
2021-08-22 11:30:54 +0800

    chore:update WriteLink.tsx

--- src/layouts/header/WriteLink.tsx
+++ src/layouts/header/WriteLink.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
 import { Create as WriteIcon } from '@material-ui/icons'
+import { observer } from 'mobx-react-lite'
+import state from '@/store/userStore'
+import { setLoginDialogVisible } from '@/store/appStore'
+import { useHistory } from 'react-router-dom'
 
 function WriteLink() {
+    const history = useHistory()
+    const toWritePage = () => {
+        if (!state.isLogin) {
+            setLoginDialogVisible(true)
+            return
+        }
+        history.push('/push')
+    }
     return (
         <Button
             style={{ marginLeft: 16 }}
             variant="contained"
             color="secondary"
             startIcon={<WriteIcon />}
+            onClick={toWritePage}
         >
             写文章
         </Button>
     )
 }
 
-export default WriteLink
+export default observer(WriteLink)
