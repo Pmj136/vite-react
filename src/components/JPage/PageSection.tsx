5b250d166a5070e2b8f1bfd438fba3617d11f088
PengMingJiu <pengmingjiu@gmail.com>
2021-08-23 09:17:31 +0800

    chore:upgrade PageSection

--- src/components/JPage/PageSection.tsx
+++ src/components/JPage/PageSection.tsx
@@ -1,13 +1,19 @@
-import React, { ReactNode } from 'react'
+import React, { CSSProperties, ReactNode } from 'react'
 
 interface IProps {
     xs: number
     children: ReactNode
+    style?: CSSProperties
 }
 
-function PageSection(props: IProps) {
-    const width = (props.xs / 12) * 100
-    return <section style={{ width: width + '%' }}>{props.children}</section>
+function PageSection({ xs, children, style }: IProps) {
+    const width = (xs / 12) * 100
+    const styleObj: CSSProperties = {
+        width: width + '%',
+        height: '100%',
+        ...style,
+    }
+    return <section style={styleObj}>{children}</section>
 }
 
 export default PageSection
