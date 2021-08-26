309df3d435b1f13bb0396b103948c4efe0f535d1
PengMingJiu <pengmingjiu@gmail.com>
2021-08-26 20:14:34 +0800

    chore:update JPage

--- src/components/JPage/Page.tsx
+++ src/components/JPage/Page.tsx
@@ -5,6 +5,7 @@ interface IProps {
     width?: number | string
     spacing?: number
     children: ReactNode
+    directionMargin?: number
 }
 
 function Page({
@@ -12,8 +13,9 @@ function Page({
     width,
     children,
     spacing = 16,
+    directionMargin = 16,
 }: IProps) {
-    const style: CSSProperties = { width }
+    const style: CSSProperties = { width, margin: `${directionMargin}px auto` }
     if (children instanceof Array) {
         style.gap = spacing + 'px'
     }
