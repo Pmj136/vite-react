b23d8bf6aa5f6cf49cb06111299fc9572124c1c5
PengMingJiu <pengmingjiu@gmail.com>
2021-08-26 20:23:39 +0800

    chore:use lazy component

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,43 +1,61 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React, { lazy, Suspense } from 'react'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import AuthRoute from './AuthRoute'
 
 import Layout from '@/layouts/Layout'
 import Home from '@/pages/home/Home'
-import Post from '@/pages/post/Post'
-import About from '@/pages/about/About'
-import NotFound from '@/components/NotFound'
-import Search from '@/pages/search/Search'
-import Notification from '@/pages/notification/Notification'
-import User from '@/pages/user/User'
-import Setting from '@/pages/setting/Setting'
-import Push from '@/pages/push/Push'
+import Loading from '@/components/Loading'
+// import Post from '@/pages/post/Post'
+// import About from '@/pages/about/About'
+// import Search from '@/pages/search/Search'
+// import User from '@/pages/user/User'
+// import Setting from '@/pages/setting/Setting'
+// import Creation from "@/pages/creation/Creation";
+const Post = lazy(() => import('@/pages/post/Post'))
+const About = lazy(() => import('@/pages/about/About'))
+const Search = lazy(() => import('@/pages/search/Search'))
+const User = lazy(() => import('@/pages/user/User'))
+const Creation = lazy(() => import('@/pages/creation/Creation'))
+const Setting = lazy(() => import('@/pages/setting/Setting'))
+const Notification = lazy(() => import('@/pages/notification/Notification'))
 
 function AppRouter() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact render={() => <Layout view={Home} />} />
-                <Route path="/post" render={() => <Layout view={Post} />} />
-                <Route path="/about" render={() => <Layout view={About} />} />
-                <Route path="/search" render={() => <Layout view={Search} />} />
-                <Route
-                    path="/user/:id/:type"
-                    render={() => <Layout view={User} />}
-                />
-                <AuthRoute authRedirect path="/push" component={Push} />
-                <AuthRoute
-                    authRedirect
-                    path="/setting/:component"
-                    render={() => <Layout view={Setting} />}
-                />
-                <AuthRoute
-                    path="/notification"
-                    render={() => <Layout view={Notification} />}
-                />
-                <Route path="*" component={NotFound} />
-            </Switch>
-        </BrowserRouter>
+        <Suspense fallback={<Loading />}>
+            <BrowserRouter>
+                <Switch>
+                    <Route
+                        path="/"
+                        exact
+                        render={() => <Layout view={Home} />}
+                    />
+                    <Route path="/post" render={() => <Layout view={Post} />} />
+                    <Route
+                        path="/about"
+                        render={() => <Layout view={About} />}
+                    />
+                    <Route
+                        path="/search"
+                        render={() => <Layout view={Search} />}
+                    />
+                    <Route
+                        path="/user/:id/:type"
+                        render={() => <Layout view={User} />}
+                    />
+                    <Route path="/creation" component={Creation} />
+                    <AuthRoute
+                        authRedirect
+                        path="/setting/:component"
+                        render={() => <Layout view={Setting} />}
+                    />
+                    <AuthRoute
+                        path="/notification"
+                        render={() => <Layout view={Notification} />}
+                    />
+                    <Redirect path="*" to="/" />
+                </Switch>
+            </BrowserRouter>
+        </Suspense>
     )
 }
 
