dbd923f5be6507581a870263230df16879d3e065
PengMingJiu <pengmingjiu@gmail.com>
2021-09-18 23:21:27 +0800

    chore:update Search

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Page from '@/components/Page'
+import { JPage, JPageSection } from '@/components/JPage'
 import Result from '@/pages/search/Result'
 
 function Search() {
     return (
-        <Page>
-            <Page.Section xs={9}>
+        <JPage>
+            <JPageSection xs={9}>
                 <Result />
-            </Page.Section>
-        </Page>
+            </JPageSection>
+        </JPage>
     )
 }
 
